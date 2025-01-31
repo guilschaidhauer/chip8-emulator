cfcb7afc024129bc838b4c13f1a5d113e9309380
guilschaidhauer <e.guilherme.castro@gmail.com>
2025-01-31 22:08:48 +0100

    Add ROM parameter

--- main.cpp
+++ main.cpp
@@ -1,10 +1,23 @@
 #include "GraphicsHandler/GraphicsHandler.h"
 #include "Chip8/Chip8.h"
+#include <iostream>
 
-int main() 
+int main(int argc, char* argv[]) 
 {
+    if (argc < 2) 
+    {
+        std::cerr << "Usage: " << argv[0] << " <path_to_rom>" << std::endl;
+        return 1; 
+    }
+
+    const char* romPath = argv[1];
+
     Chip8 chip8;
-    chip8.loadRom("roms/testProgram.ch8");
+    if (!chip8.loadRom(romPath)) 
+    {
+        std::cerr << "Failed to load ROM: " << romPath << std::endl;
+        return 1; 
+    }
 
     GraphicsHandler graphicsHandler;
     graphicsHandler.init();
